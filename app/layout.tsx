3cd987c07411f84ccdc72c35914c19af68a166dd
softsiliconinfotech <softsiliconinfotech@gmail.com>
2026-08-28 19:09:28 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,14 +16,14 @@ const outfit = Outfit({
 });
 
 export const metadata: Metadata = {
-  title: "SiliconSoft Infotech LLP | Best Software Development & IT Services | Web, Mobile Apps & AI",
-  description: "SoftSillicon Infotech LLP (formerly NP IT Solutions) is a top-rated software development company... Infotech LLP provides industry-leading IT services, including Web & Mobile App Development, AI, Cloud Computing, and Custom Software Solutions for Healthcare, Real Estate, E-Commerce, Agri-Tech, and more.",
-  keywords: ["SiliconSoft Infotech LLP", "Software Development", "Website Design Nashik", "Digital Marketing Agency Nashik", "Web Developers Nashik", "App Developers Nashik"],
+  title: "Softsilicon Infotech LLP | Best Software Development & IT Services | Web, Mobile Apps & AI",
+  description: "SoftSillicon Infotech LLP is a top-rated software development company... Infotech LLP provides industry-leading IT services, including Web & Mobile App Development, AI, Cloud Computing, and Custom Software Solutions for Healthcare, Real Estate, E-Commerce, Agri-Tech, and more.",
+  keywords: ["Softsilicon Infotech LLP", "Software Development", "Website Design Nashik", "Digital Marketing Agency Nashik", "Web Developers Nashik", "App Developers Nashik"],
   openGraph: {
-    title: "SiliconSoft Infotech LLP | Best Software Development & IT Services",
+    title: "Softsilicon Infotech LLP | Best Software Development & IT Services",
     description: "Industry-leading IT services, including Web & Mobile App Development, AI, Cloud Computing, and Custom Software Solutions.",
-    url: "https://SiliconSoftsolutions.in",
-    siteName: "SiliconSoft Infotech LLP",
+    url: "https://softsiliconinfotech.com",
+    siteName: "Softsilicon Infotech LLP",
     locale: "en_US",
     type: "website",
   },
